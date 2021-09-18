f74d47d3a2d3580a15339209fe7c7c9af4413d28
mohanish <mohanishbait@gmail.com>
2021-09-18 13:48:43 +0530

    corona web side folder download

---
+++ scripts/testingscript.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCfaCPcjO7sCK1AkbZtWXlChmXyygQjdjg",
+    authDomain: "corona-testing-b6ae0.firebaseapp.com",
+    databaseURL: "https://corona-testing-b6ae0-default-rtdb.firebaseio.com",
+    projectId: "corona-testing-b6ae0",
+    storageBucket: "corona-testing-b6ae0.appspot.com",
+    messagingSenderId: "402599811440",
+    appId: "1:402599811440:web:ed666853f9b0f2749697b5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
